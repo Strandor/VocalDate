ee89030951e0c94ba9b362f318999f25671b98fe
Steinar <steinar1smari@gmail.com>
2022-04-16 23:58:50 +0000

    Setting up github actions

--- Infastructure/main.tf
+++ Infastructure/main.tf
@@ -21,14 +21,12 @@ provider "google" {
   zone    = "us-east1-a"
 }
 
-resource "google_sql_database_instance" "main" {
-  name             = "main-instance"
+resource "google_sql_database_instance" "app" {
+  name             = "app"
   database_version = "POSTGRES_11"
   region           = "us-east1"
 
   settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
     tier = "db-f1-micro"
   }
 }
