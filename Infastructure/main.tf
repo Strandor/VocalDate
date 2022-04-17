81ebdf428536e84528952851a0f0006a67a3205d
Steinar <steinar1smari@gmail.com>
2022-04-17 02:18:27 +0000

    Update action

--- Infastructure/main.tf
+++ Infastructure/main.tf
@@ -33,7 +33,7 @@ resource "google_cloud_run_service" "default" {
   template {
     spec {
       containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
+        image = "gcr.io/vocaldate/web:latest"
       }
     }
   }
