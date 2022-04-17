8ed696314e67d5043e1e0d262e460201b3322eb9
Steinar <steinar1smari@gmail.com>
2022-04-17 02:44:19 +0000

    Update action

--- Infastructure/main.tf
+++ Infastructure/main.tf
@@ -26,6 +26,23 @@ resource "google_sql_database_instance" "app" {
   }
 }
 
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.default.location
+  project     = google_cloud_run_service.default.project
+  service     = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
 resource "google_cloud_run_service" "default" {
   name     = "cloudrun-srv"
   location = "us-east1"
